2e4b1f0a190f24122eba1cde82cae43764115b04
lennard-pfennig-bruker <lennard.pfennig@bruker.com>
2022-06-09 18:42:10 +0200

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.18
+
+require github.com/lennard-pfennig-bruker/go-lib v1.0.1
---
+++ mystuff.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/lennard-pfennig-bruker/go-lib/lennard"
+)
+
+func main() {
+	lennard.Run()
+}
